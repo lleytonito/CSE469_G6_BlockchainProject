7dccd78ef9d7565cea5d8b2da4c471ba43bab48e
obliviamon <obliviamon@gmail.com>
2023-04-03 14:10:07 -0700

    init Command implemented for local and final testing
    May need additional tuning once add and remove have been added. We could simply rewrite the entire file at the end of the program's running, but we may lose data if it crashes mid-session that way.

--- bchoc.py
+++ bchoc.py
@@ -1,7 +1,105 @@
 import struct
 import sys
 import argparse
+import time
 import uuid
+import os
+
+#Check datapath to file for init command later
+filepath = os.environ.get('BCHOC_FILE_PATH', 'bchoc.bin')
+
+###################################################################################################
+#packFormatHeader is the format recommended by the professor, used as basis for dynamic actual format in packFormatALL
+packFormatHeader = '32s d 16s I 12s I'
+
+#These lists are 'synced': each index corresponds to one another, mainly a failsafe for if we need a specific block's format later
+formatList = []
+blockList = []
+
+#take all inputs (EXCEPT SIZE, WHICH WILL BE DYNAMICALLY CALCULATED) to create a block and append to the blockList
+#First input is boolean True or False for whether the new object should be written to the file. Should be True in most cases
+def packFormatAll(writeToFile, prevHash, time, caseID, evidenceID, state, data):
+    #get length of data to calculate size
+    dataLength = len(data.encode('utf-8'))
+    #define new format using size of the data as the last string field's length
+    currentBlockFormat = packFormatHeader + ' ' + str(dataLength) + 's'
+    #append the new format to formatList
+    formatList.append(currentBlockFormat)
+    #create a new byte struct of the specified format, and append it to blockList. also return in case it needs immediate use
+    newBlock = struct.pack(currentBlockFormat, bytes(prevHash, 'utf-8'), time, bytes(caseID, 'utf-8'), evidenceID, bytes(state, 'utf-8'), dataLength, bytes(data, 'utf-8'))
+    blockList.append(newBlock)
+    if (writeToFile):
+        fileToWrite = open(filepath, 'ab')
+        fileToWrite.write(newBlock)
+        fileToWrite.close()
+    return newBlock
+
+def unpackFromList(index):
+    currentBlockFields = []
+
+    #Use index to grab the correct object, then find the last index in it using len
+    currentBlock = blockList[index]
+    lastIndex = len(currentBlock)
+
+    #this just simplifies later sections, each field is assigned its own bytes variable for conversion
+    bytesPrevHash = bytes(currentBlock[0:31])
+    bytesTime = bytes(currentBlock[32:40])
+    bytesCaseID = bytes(currentBlock[40:55])
+    bytesEvidenceID = bytes(currentBlock[56:59])
+    bytesState = bytes(currentBlock[60:71])
+    bytesSize = bytes(currentBlock[72:75])
+    bytesData = bytes(currentBlock[76:lastIndex])
+
+
+    #convert to human-readable format for each field
+    prevHash = (bytesPrevHash.decode('utf-8'))
+    time = ((struct.unpack('d', bytesTime))[0])
+    caseID = (bytesCaseID.decode('utf-8'))
+    evidenceID = (int.from_bytes(bytesEvidenceID, sys.byteorder))
+    state = (bytesState.decode('utf-8'))
+    size = (int.from_bytes(bytesSize, sys.byteorder))
+    data = (bytesData.decode('utf-8'))
+
+    #add all of these fields to a list and then return it (not sure what we should really be doing here so I thought this was a good placeholder)
+    currentBlockFields.append(prevHash)
+    currentBlockFields.append(time)
+    currentBlockFields.append(caseID)
+    currentBlockFields.append(evidenceID)
+    currentBlockFields.append(state)
+    currentBlockFields.append(size)
+    currentBlockFields.append(data)
+
+    return currentBlockFields
+
+#This function is used to read from the file in init
+def unpackFromFile(file, blockOffset, size):
+    #Use size argument to find last index for specific block
+    lastIndex = (75+size)
+    
+    #this just simplifies later sections, each field is assigned its own bytes variable for conversion
+    bytesPrevHash = bytes(file[0:blockOffset+31])
+    bytesTime = bytes(file[blockOffset+32:blockOffset+40])
+    bytesCaseID = bytes(file[blockOffset+40:blockOffset+55])
+    bytesEvidenceID = bytes(file[blockOffset+56:blockOffset+59])
+    bytesState = bytes(file[blockOffset+60:blockOffset+71])
+    bytesSize = bytes(file[blockOffset+72:blockOffset+75])
+    bytesData = bytes(file[blockOffset+76:blockOffset+lastIndex])
+
+
+    #convert to human-readable format for each field
+    prevHash = (bytesPrevHash.decode('utf-8'))
+    time = ((struct.unpack('d', bytesTime))[0])
+    caseID = (bytesCaseID.decode('utf-8'))
+    evidenceID = (int.from_bytes(bytesEvidenceID, sys.byteorder))
+    state = (bytesState.decode('utf-8'))
+    size = (int.from_bytes(bytesSize, sys.byteorder))
+    data = (bytesData.decode('utf-8'))
+
+    #Pass arguments to packFormatAll with writeToFile = False so the objects are not duplicated in the file
+    packFormatAll(False, prevHash, time, caseID, evidenceID, state, data)
+
+###################################################################################################
+
 
 #add command implementation
 def add_command(args):
@@ -47,7 +145,36 @@ def remove_command(args):
 
 #init command implementation
 def init_command():
-    print("Init Command")
+    #offset used for reading more than 1 block from file when they exist
+    offset = 0
+    #check if the file specified by the check at the top of program exists
+    if (os.path.isfile(filepath)):
+        #if it does, open the file in read-binary mode
+        with open(filepath, 'rb') as file:
+            #create a bytearray to read in the file's data
+            existingBlocks = bytearray()
+            existingBlocks = file.read()
+            #check to see if the first block has a size greater than 0 (should be 14 if it is there)
+            bytesSize = existingBlocks[72:75]
+            size = (int.from_bytes(bytesSize, sys.byteorder))
+            #if so, while the size of each block in the file is not 0
+            if (size != 0):
+                while (size != 0):
+                    #read the block from the file at the current offset into our arrays that store data structures
+                    unpackFromFile(existingBlocks, offset, size)
+                    #increment offset according to base size + size of data string at end of struct
+                    offset = offset + 75 + size
+                    #get the new size for the next while loop check
+                    bytesSize = existingBlocks[offset+72:offset+75]
+                    size = (int.from_bytes(bytesSize, sys.byteorder))
+            #if the file is empty for some reason, add the initial block as specified in instructions
+            else:
+                initialBlock = packFormatAll(True, '', time.time(), '', 0, 'INITIAL', 'Initial Block')
+
+    #if the file does not exist, create it and add the initial block as specified in instructions
+    else:
+        bcFile = open(filepath, 'wb')
+        initialBlock = packFormatAll(True, '', time.time(), '', 0, 'INITIAL', 'Initial Block')
 
 #verify command implementation
 def verify_command():
@@ -112,74 +239,3 @@ elif args.command == "verify":
 else:
     print("Invalid command")
     parser.print_help()
-
-
-
-#packFormatHeader is the format recommended by the professor, used as basis for dynamic actual format in packFormatALL
-packFormatHeader = '32s d 16s I 12s I'
-
-#These lists are 'synced': each index corresponds to one another, mainly a failsafe for if we need a specific block's format later
-formatList = []
-blockList = []
-
-#take all inputs (EXCEPT SIZE, WHICH WILL BE DYNAMICALLY CALCULATED) to create a block and append to the blockList
-def packFormatAll(prevHash, time, caseID, evidenceID, state, data):
-    #get length of data to calculate size
-    dataLength = len(data.encode('utf-8'))
-    #define new format using size of the data as the last string field's length
-    currentBlockFormat = packFormatHeader + ' ' + str(dataLength) + 's'
-    #append the new format to formatList
-    formatList.append(currentBlockFormat)
-    #create a new byte struct of the specified format, and append it to blockList. also return in case it needs immediate use
-    newBlock = struct.pack(currentBlockFormat, bytes(prevHash, 'utf-8'), time, bytes(caseID, 'utf-8'), evidenceID, bytes(state, 'utf-8'), dataLength, bytes(data, 'utf-8'))
-    blockList.append(newBlock)
-    return newBlock
-
-def unpackFromList(index):
-    currentBlockFields = []
-
-    #Use index to grab the correct object, then find the last index in it using len
-    currentBlock = blockList[index]
-    lastIndex = len(currentBlock)
-
-    #this just simplifies later sections, each field is assigned its own bytes variable for conversion
-    bytesPrevHash = bytes(currentBlock[0:31])
-    bytesTime = bytes(currentBlock[32:40])
-    bytesCaseID = bytes(currentBlock[40:55])
-    bytesEvidenceID = bytes(currentBlock[56:59])
-    bytesState = bytes(currentBlock[60:71])
-    bytesSize = bytes(currentBlock[72:75])
-    bytesData = bytes(currentBlock[76:lastIndex])
-
-
-    #convert to human-readable format for each field
-    prevHash = (bytesPrevHash.decode('utf-8'))
-    time = ((struct.unpack('d', bytesTime))[0])
-    caseID = (bytesCaseID.decode('utf-8'))
-    evidenceID = (int.from_bytes(bytesEvidenceID, sys.byteorder))
-    state = (bytesState.decode('utf-8'))
-    size = (int.from_bytes(bytesSize, sys.byteorder))
-    data = (bytesData.decode('utf-8'))
-
-    #add all of these fields to a list and then return it (not sure what we should really be doing here so I thought this was a good placeholder)
-    currentBlockFields.append(prevHash)
-    currentBlockFields.append(time)
-    currentBlockFields.append(caseID)
-    currentBlockFields.append(evidenceID)
-    currentBlockFields.append(state)
-    currentBlockFields.append(size)
-    currentBlockFields.append(data)
-
-    return currentBlockFields
-
-print()
-print()
-print("BLOCKCHAIN TESTS")
-print()
-
-#example prints, just here for if anyone wants to mess around with testing
-print(packFormatAll('abcd', 12.1, '14a', 300, '40a', 'testStringofIndeterminateLength'))
-print()
-print(unpackFromList(0))
-
-
